1f107761577ebfe4e1b62fdedd8610761d0cafbb
도엽 김 <kkamangdols@naver.com>
2022-09-20 10:35:18 +0900

    윈폼 꾸미기 추가

--- CuteCat/Form1.Designer.cs
+++ CuteCat/Form1.Designer.cs
@@ -29,40 +29,12 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.CatScreen = new System.Windows.Forms.Label();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.CatTimer = new System.Windows.Forms.Timer(this.components);
             this.Play = new System.Windows.Forms.Button();
             this.Feed = new System.Windows.Forms.Button();
-            this.CatTimer = new System.Windows.Forms.Timer(this.components);
+            this.CatScreen = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // CatScreen
-            // 
-            this.CatScreen.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.CatScreen.Location = new System.Drawing.Point(27, 36);
-            this.CatScreen.Name = "CatScreen";
-            this.CatScreen.Size = new System.Drawing.Size(1345, 143);
-            this.CatScreen.TabIndex = 0;
-            this.CatScreen.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            // 
-            // Play
-            // 
-            this.Play.Location = new System.Drawing.Point(39, 246);
-            this.Play.Name = "Play";
-            this.Play.Size = new System.Drawing.Size(191, 127);
-            this.Play.TabIndex = 1;
-            this.Play.Text = "Play";
-            this.Play.UseVisualStyleBackColor = true;
-            this.Play.Click += new System.EventHandler(this.Play_Click);
-            // 
-            // Feed
-            // 
-            this.Feed.Location = new System.Drawing.Point(285, 246);
-            this.Feed.Name = "Feed";
-            this.Feed.Size = new System.Drawing.Size(191, 127);
-            this.Feed.TabIndex = 2;
-            this.Feed.Text = "Feed";
-            this.Feed.UseVisualStyleBackColor = true;
-            this.Feed.Click += new System.EventHandler(this.Feed_Click);
             // 
             // CatTimer
             // 
@@ -70,25 +42,60 @@
             this.CatTimer.Interval = 5000;
             this.CatTimer.Tick += new System.EventHandler(this.CatTimer_Tick);
             // 
+            // Play
+            // 
+            this.Play.Location = new System.Drawing.Point(45, 625);
+            this.Play.Name = "Play";
+            this.Play.Size = new System.Drawing.Size(191, 142);
+            this.Play.TabIndex = 1;
+            this.Play.Text = "Play";
+            this.Play.UseVisualStyleBackColor = true;
+            this.Play.Click += new System.EventHandler(this.Play_Click);
+            // 
+            // Feed
+            // 
+            this.Feed.Location = new System.Drawing.Point(45, 799);
+            this.Feed.Name = "Feed";
+            this.Feed.Size = new System.Drawing.Size(191, 144);
+            this.Feed.TabIndex = 2;
+            this.Feed.Text = "Feed";
+            this.Feed.UseVisualStyleBackColor = true;
+            this.Feed.Click += new System.EventHandler(this.Feed_Click);
+            // 
+            // CatScreen
+            // 
+            this.CatScreen.BackColor = System.Drawing.Color.Transparent;
+            this.CatScreen.Font = new System.Drawing.Font("Showcard Gothic", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.CatScreen.Location = new System.Drawing.Point(349, 9);
+            this.CatScreen.Name = "CatScreen";
+            this.CatScreen.Size = new System.Drawing.Size(918, 237);
+            this.CatScreen.TabIndex = 0;
+            this.CatScreen.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1404, 1079);
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1627, 1002);
             this.Controls.Add(this.Feed);
             this.Controls.Add(this.Play);
             this.Controls.Add(this.CatScreen);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private Label CatScreen;
+        private System.Windows.Forms.Timer CatTimer;
         private Button Play;
         private Button Feed;
-        private System.Windows.Forms.Timer CatTimer;
+        private Label CatScreen;
     }
 }
